28dd09e72a9702e3fa7a15af16ab9345fd2e4377
javkhlanturg <javkhlantur.g@gmail.com>
2016-10-17 19:38:20 +0800

    demii

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import Stateful from './stateful.jsx';
 import Props from './props.jsx';
 import StateAndProps from './stateandprops.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+//ReactDOM.render(<App />, document.getElementById('app'));
 ReactDOM.render(<Stateful />, document.getElementById('stateful'));
 ReactDOM.render(<Props headerProp = "Header from props.. " contentProp = "Content from props"/>, document.getElementById('props'));
 ReactDOM.render(<StateAndProps />, document.getElementById('stateandprops'));
